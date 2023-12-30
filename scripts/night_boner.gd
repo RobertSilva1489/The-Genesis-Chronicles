8a45b1d57e4c434996a15ff9dfa875bdab6c2b4e
RobertSilva1489 <lionelrobert7@gmail.com>
2023-12-30 11:50:32 -0300

    night boner done

--- scripts/night_boner.gd
+++ scripts/night_boner.gd
@@ -8,7 +8,8 @@ extends CharacterBody2D
 @export var follow = false
 @export var attack_cooldown : float = 1.5
 @export var attack_player = false
-@export var DIST_FOLLOW := 300
+@export var DIST_FOLLOW := 500
+@export var DIST_ATTACK := 200
 @onready var leaf: CharacterBody2D = $"../Leaf"
 var distance := 0.0
 var _position
@@ -18,6 +19,11 @@ var dano: int
 
 func _ready() -> void:
 	$TextureProgressBar.value = health
+func _physics_process(delta: float) -> void:
+	if !is_on_floor():
+		velocity.y += gravity * delta
+		move_and_slide()
+
 func _process(delta: float) -> void:
 	if leaf != null:
 		distance = global_position.distance_to(leaf.global_position)
@@ -26,19 +32,22 @@ func _process(delta: float) -> void:
 	else:
 		follow = false 
 		$AnimationPlayer.play("idle")
+	if distance <=DIST_ATTACK:
+		attack_player = true
+	else:
+		attack_player = false
 	if leaf != null and dead == false:
 		_position = leaf.global_position.x - global_position.x
 		direction = sign(_position)
 		if attack_player and can_attack:
 			attack()
 		elif follow and attack_player !=true:
-			$AnimationPlayer.play("idle")
+			$AnimationPlayer.play("walk")
 			_patrol()
 	_flip()	
 	$TextureProgressBar.value = health
 func blink() -> void:
 	$Marker2D/Sprite2D.modulate = Color(10,10,10,10)
-	$AnimationPlayer.play("hit")
 	await get_tree().create_timer(.1).timeout
 	$Marker2D/Sprite2D.modulate = Color(1,1,1,1)
 func damage (dame) -> void:
@@ -57,36 +66,22 @@ func _patrol():
 	if Global.health > 0 and health > 0:
 		velocity.x = direction * speed
 		move_and_slide()
-	if global_position.y != leaf.global_position.y:
-		if global_position.y < leaf.global_position.y:
-			velocity.y = 50
-		if global_position.y > leaf.global_position.y:
-			velocity.y = -50
 	if follow == false:
 		velocity.x = 0 
 		
 func _flip() -> void:
 	if direction < 0 and health > 0 and $Timer.time_left <=0:
 		transform.x.x = -1
-		$Timer.start(0.5)
+		$Timer.start(0.7)
 	if direction > 0 and health > 0 and $Timer.time_left <=0:
-		transform.x.x = 1
-		$Timer.start(0.5)	
+		transform.x.x = 1 
+		$Timer.start(0.7)	
 func attack():
 	can_attack = false
 	$AnimationPlayer.play("attack")
 	await get_tree().create_timer(attack_cooldown).timeout
 	can_attack = true
 	
-func _on_detect_player_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") and Global.health > 0:
-		attack_player = true	
-
-func _on_detect_player_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		attack_player = false
-
-
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if health > 0 and dead == false:
 		$AnimationPlayer.play("idle")

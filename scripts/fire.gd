e38a2f81a24c76adfb2fbbcf9583d57e15a8d617
RobertSilva1489 <lionelrobert7@gmail.com>
2024-03-04 16:11:09 -0300

    bug resolved

--- scripts/fire.gd
+++ scripts/fire.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 var attack_hold = 1
-var attacks = ["attack1","attack2","attack3","defender","defender"]
-var special = ["special1","special2","defender","defender"]
+var attacks = ["attack1","attack2","attack3","defender"]
+var special = ["special1","special2","defender"]
 var special2 = ["special1","special2","attack1","attack2","attack3"]
 var direction = 0
 var dead = false
@@ -10,11 +10,14 @@ var distance
 var move :int
 var selected_animation
 var damage_apply = randi_range(15,30)
+var _position
+@export var DIST_FOLLOW := 1000
+@export var DIST_ATTACK := 50
 @export var invencible = false
 @export var speed = 70
 @export var health = 100
 @export var gravity = 3000
-@export var attack_cooldown : float = 1.0
+@export var attack_cooldown : float = 1.2
 @export var countHit = 0
 @export var attack_player = false
 @export var can_attack = true
@@ -35,13 +38,23 @@ func _physics_process(delta: float) -> void:
 
 func _process(delta: float) -> void:
 	Global.boss_health = health
+	if leaf != null:
+		distance = global_position.distance_to(leaf.global_position)
+		if distance <=DIST_FOLLOW:
+			follow = true
+		else:
+			follow = false
+			$AnimationPlayer.play("idle")
+		if distance <=DIST_ATTACK:
+			attack_player = true
+		else:
+			attack_player = false
 	if leaf != null and dead == false:
-		distance = leaf.global_position.x - global_position.x
-		direction = sign(distance)
-		invencible = false
+		_position = leaf.global_position.x - global_position.x
+		direction = sign(_position)
 		if attack_player and can_attack:
 			attack()
-		if follow and attack_player !=true:
+		elif follow and attack_player !=true:
 			$AnimationPlayer.play("run")
 			_patrol()
 	_flip()	
@@ -62,21 +75,21 @@ func _flip() -> void:
 		$Timer.start(0.5)
 func attack():
 	if dead == false:
-		print("ATAQUE")
 		var attack_animations
 		var selected_animation
 		if countHit < 7 and health > 30:
 			attack_animations = attacks
 		elif countHit >= 7 and health > 30:
 			attack_animations = special
-		elif  health < 30:
+		elif  health < 30 and health > 0:
 			attack_animations = special2
 		for attackSet in attack_animations:
 			selected_animation = attackSet
 			can_attack = false
-			$AnimationPlayer.play(selected_animation)
-	#		await get_tree().create_timer(attack_cooldown).timeout
-			$Timer.start()
+			if dead == false:
+				$AnimationPlayer.play(selected_animation)
+				await get_tree().create_timer(attack_cooldown).timeout
+				can_attack = true
 func damage (dame) -> void:
 	if invencible !=true:
 		blink()
@@ -88,14 +101,10 @@ func damage (dame) -> void:
 		gravity = 0
 		Global.show_boss = false
 		can_attack = false
-		leaf = null
 		$CollisionShape2D.shape = null
-		$detectPlayer/CollisionShape2D.shape = null
-		$followPlayer/CollisionShape2D.shape = null
 		$AnimationPlayer.speed_scale = 1
 		$AnimationPlayer.play("death")
 		await $AnimationPlayer.animation_finished
-		print("Dead? ",$AnimationPlayer.current_animation)
 	
 func blink() -> void:
 	$Sprite2D.modulate = Color(10,10,10,10)
@@ -105,27 +114,9 @@ func _on_attacks_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.take_damage(damage_apply)
 
-func _on_attack_player_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		attack_player = true
-		
-func _on_attack_player_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		attack_player = false
-		
-func _on_follow_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		follow = true
-
-func _on_follow_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		follow = false
-
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if health > 0 and dead == false:
 		$AnimationPlayer.play("idle")
-
-
-func _on_timer_timeout() -> void:
-	print("Ataque ai va ")
-	can_attack = true
+	if dead == true:
+		$AnimationPlayer.play("death")
+	

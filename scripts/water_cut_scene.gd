1b0d9693747eb1ab844d5f9e0c041ce2456ab80d
RobertSilva1489 <lionelrobert7@gmail.com>
2024-04-02 16:15:05 -0300

    all open scene done

---
+++ scripts/water_cut_scene.gd
@@ -0,0 +1,8 @@
+extends Node2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Button.grab_focus()
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/world_water.tscn")
+func _on_animation_player_2_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_file("res://scene/world_water.tscn")

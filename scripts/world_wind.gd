24dcc31f57e356ff70ad0ac9fec3174f8cf8313d
RobertSilva1489 <lionelrobert7@gmail.com>
2024-04-26 16:42:45 -0300

    wind world
    beginning to finish world wind

--- scripts/world_wind.gd
+++ scripts/world_wind.gd
@@ -1,14 +1,63 @@
 extends Node2D
 
+var night_boner = preload("res://scene/night_boner.tscn")
+var monster = preload("res://scene/monster.tscn")
+var boss = preload("res://scene/ground.tscn")
+var ground_check = true
+@onready var leaf: CharacterBody2D = $Leaf
+@export var wave_lenght = 30
+@export var sceneEnemy = 6
+@onready var spawn1 = $spawn
+@onready var spawn2 = $spawn2
+@onready var spawn3 = $spawn3
+@onready var spawn4 = $spawn4
+@onready var boss_spawn = $spawn5
+var RanPos = []
+var enemy = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.showwave = true
-
+	Global.wave = wave_lenght
+	Global.mana = 100
+	Global.quiver = 10
+	$"/root/Hud".show()
+	spawn_enemy()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var ground: CharacterBody2D = $ground
 	if Global.wave == 0:
-		$AnimationPlayer.play("new_animation")
-		Global.showwave = false
 		Global.wave = -1
+		$AnimationPlayer.play("open")
+		Global.showwave = false
+	if ground != null:
+		if ground.health <=0 and ground_check == true:
+			ground_check = false
+#			$level.stream_paused = true
+#			$bossDead.play()
+			leaf.powerUP("ground")
+			await get_tree().create_timer(5).timeout
+			leaf._out()
+func spawn_enemy():
+	wave_lenght-=1
+	Global.scene_enemy+=1
+	randomize()
+	RanPos.append(spawn1)
+	RanPos.append(spawn2)
+	RanPos.append(spawn1)
+	RanPos.append(spawn2)
+	RanPos.append(spawn3)
+	RanPos.append(spawn4)
+	enemy.append(night_boner)
+	enemy.append(monster)
+	enemy.append(monster)
+	var selecte = enemy.pick_random()
+	var select = RanPos.pick_random()
+	var Enemy = selecte.instantiate()
+	add_child(Enemy)
+	Enemy.global_position = select.global_position
+
+func spawn_boss():
+	var Boss = boss.instantiate()
+	add_child(Boss)
+	Boss.global_position = boss_spawn.global_position

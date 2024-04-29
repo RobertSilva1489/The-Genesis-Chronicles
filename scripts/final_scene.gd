cfb161e7a6c6684fc698cda22d6f37aa5530d84d
RobertSilva1489 <lionelrobert7@gmail.com>
2024-04-29 19:44:18 -0300

    start doing NPC dialogue

--- scripts/final_scene.gd
+++ scripts/final_scene.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
-
+var play = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$VBoxContainer/menu.grab_focus()
 
+func _on_menu_pressed() -> void:
+	$pressed.play()
+	await $pressed.finished
+	get_tree().change_scene_to_file("res://scene/main_menu.tscn")
+func _on_menu_focus_entered() -> void:
+	if not play:
+		$focus.play()
+	play = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_exit_pressed() -> void:
+	$pressed.play()
+	await $pressed.finished
+	get_tree().quit()
+func _on_exit_focus_entered() -> void:
+	$focus.play()
